519bb09b6721b59076eae8aaa91d76981ae984d2
c.ilave_ofape_37 <c.ilave.barboza@gmail.com>
2024-04-26 16:33:59 -0500

    change pc

--- script_1.py
+++ script_1.py
@@ -14,7 +14,8 @@ buoys = [
         '0n170w', '2n170w', '5n170w', '8n170w', '2s170w', '5s170w', '8s170w'
         ]
 buoys = [
-        '5n165e', '8n165e', '2s165e', '5s165e', '8s165e',
+        '0n147e', '2n147e', '5n147e', '8n147e', '2s147e', '5s147e', '8s147e',
+        '0n165e', '2n165e', '5n165e', '8n165e', '2s165e', '5s165e', '8s165e',
         '0n156e', '2n156e', '5n156e', '8n156e', '2s156e', '5s156e', '8s156e',
         '0n180w', '2n180w', '5n180w', '8n180w', '2s180w', '5s180w', '8s180w',
         '0n170w', '2n170w', '5n170w', '8n170w', '2s170w', '5s170w', '8s170w'
@@ -23,8 +24,10 @@ for buoy in buoys:
         
     year_hist = '1990'
 
-    for year in ['1991', '1992', '1997', '2015', '2016', '2017', '2023']:
+    for year in ['1991', '1992', '1997', '1998',  '1999', '2000', '2015', '2016', '2017', '2023']:
         try:
+            
+            print(f'{buoy} - {year}')
                 
             rqw.init_wind_data_download(buoy)
 
@@ -40,9 +43,9 @@ for buoy in buoys:
             a[:] = np.nan
 
 
-            data_hist['wsu'] = [cws.calc_wind_stress(u, 1022) for u in data_hist['winds_u']] 
+            data_hist['wsu'] = [cws.calc_wind_stress(u, 1.2) for u in data_hist['winds_u']] 
 
-            data_year['wsu'] = [cws.calc_wind_stress(u, 1022) for u in data_year['winds_u']]
+            data_year['wsu'] = [cws.calc_wind_stress(u, 1.2) for u in data_year['winds_u']]
             data_year['anom_u'] = data_year['winds_u'] - data_hist['winds_u']
             data_year['anom_wsu'] = data_year['wsu'] - data_hist['wsu']
 
@@ -61,11 +64,11 @@ for buoy in buoys:
             ax[2].barh(data_year[data_year['anom_wsu']>=0]['time2'], data_year[data_year['anom_wsu']>=0]['anom_wsu'], color='red')
 
             for axi in ax:
-                axi.set_xbound(-125, 125)
+                axi.set_xbound(-0.16, 0.16)
                 axi.set_ybound('01/01', '12/31')
                 axi.set_yticks(['01-01', '02-01', '03-01', '04-01', '05-01', '06-01', '07-01', '08-01', '09-01', '10-01', '11-01', '12-01', '12-31'], ['ENE', 'FEB', 'MAR', 'ABR', 'MAY', 'JUN', 'JUL', 'AGO', 'SEP', 'OCT', 'NOV', 'DIC', 'ENE'])
                 axi.set_xlabel('Wind Stress (N/m2)')
-                axi.set_xticks(np.arange(-100, 100 + 1, 25), np.arange(-100, 100 + 1, 25), fontsize= 10)
+                axi.set_xticks(np.arange(-0.16, 0.16 + 0.01, 0.02), [-0.16, '', -0.12, '', -0.08, '', -0.04, '', 0, '', 0.04, '', 0.08, '', 0.12, '', 0.16], fontsize= 8)
                 axi.grid()
 
             ax[0].set_ylabel('Fecha')
